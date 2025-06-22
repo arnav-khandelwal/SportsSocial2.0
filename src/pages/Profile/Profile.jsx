83d7f60552449e4d84f6cb24e1ba4b3f968401ae
Arnav Khandelwal <52711982+arnav-khandelwal@users.noreply.github.com>
2025-06-22 15:14:46 +0530

    Updated Profile.jsx

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -542,7 +542,7 @@ const Profile = () => {
               onClick={() => handleImageUpload('profile_picture')}
               disabled={uploadingImage}
             >
-      <FaEdit style={{ fontSize: 20 }} />
+          <FaEdit style={{ fontSize: 20 }} />
             </button>
           )}
         </div>
